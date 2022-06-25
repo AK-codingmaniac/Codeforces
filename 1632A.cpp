4589f663eaa0b2c2c37837ec4dbceb18401daf3d
Anshaj Khare <anshaj13.ugcs20@iiitranchi.ac.in>
2022-06-25 09:49:56 +0530

    Day 42

---
+++ 1632A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T>0)
+    {
+        int n;
+        cin>>n;
+        string str;
+        cin>>str;
+        if((n>2) || (str=="11") || (str=="00"))
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+        T--;
+    }
+    return 0;
+}
